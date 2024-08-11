f26c5572ac4994b5b7e06c5506b8eb2ab5967e44
Dominic.Fryman <fryman.dominic@gmail.com>
2024-08-11 19:04:14 -0400

    Add comment for getHumanChoice

--- javascript.js
+++ javascript.js
@@ -1,8 +1,11 @@
+
+
 // Returns 0, 1, or 2 corresponding to rock, paper, or scissor
 function getComputerChoice() {
     return Math.floor(Math.random() * 3);
 }
 
+// Returns a string corresponding to rock, paper, or scissor
 function getHumanChoice() {
     return prompt("Rock, paper, or scissors?");
 }
